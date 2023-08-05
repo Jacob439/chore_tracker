23cc47fc954df9488f685b241dbdadc03486b4c7
Jacob439 <jakesisawesome1@gmail.com>
2023-08-05 19:48:25 -0400

    add cool transition

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import confetti from 'canvas-confetti';
+// import confetti from 'canvas-confetti';
 
 function App() {
   // TEMP ARRAY:
@@ -22,13 +22,30 @@ function App() {
         <p>
           turn to unload the dishwasher
         </p>
-        <button class="button_finished" onClick={() => {
-          name = kids[((counter++) % 4)];
-          console.log(name);
-          confetti();
-          document.getElementById("turn_name").innerHTML = "<h1>" + name + "'s";
+        <button class="button_finished" onClick={(e) => {
+          const button = document.querySelector('.button_finished');
+          e.preventDefault();
+          button.classList.add('button_finished--clicked');
+          document.querySelectorAll('span').forEach((element) => { element.classList.add('expanded') })
+          /* 
+      ---------------------------------------
+      just to reset without having to refresh..
+       --------------------------------*/
+          setTimeout(() => { button.classList.remove("button_finished--clicked") }, 3500);
+          // Now insert new page here
+          // and REMOVE BELOW LINE
+          setTimeout(() => { document.querySelectorAll('span').forEach((element) => { element.classList.remove('expanded') }) }, 1700)
+          // name = kids[((counter++) % 4)];
+          // console.log(name);
+          // confetti();
+          // document.getElementById("turn_name").innerHTML = "<h1>" + name + "'s";
         }}>I finished unloading it!</button>
       </header>
+      <span class="color color--blue" data-value="1"></span>
+      <span class="color color--orange" data-value="1"></span>
+      <span class="color color--green" data-value="1"></span>
+      <span class="color color--white" data-value="1">
+      </span>
     </div>
   );
 }
